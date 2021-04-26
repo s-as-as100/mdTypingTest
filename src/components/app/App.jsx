74ae5c722fe8b94504a7e919088c26270d83db30
s-as-as100 <s.arifsaifi000@gmail.com>
2021-04-27 02:55:39 +0530

    logic of our typing speed test

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,77 +4,202 @@ import './App.css';
 import Landing from './landing/landing';
 import Footer from './footer/footer';
 import ChallengeSection from './challengeSection/challengeSection';
-
-const apiURl ="https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=text";
-const timerStarted = 30;
-class App extends React.Component {
-    state = {
-        selectedParagraph: "hi",
-        timerStarted: false,
-        testInfo: [],
-        timeRemaining: timerStarted,
-        words: 0 ,
-        characters:0,
-        wpm:0,
-        onInputChange
  
+/**
+ * Schema of Test Info:
+ * [
+ *    {
+ *      testLetter: 'H',
+ *      status: correct/incorrect/notAttempted
+ *    }, {
+ *      testLetter: 'e',
+ *      status: correct/incorrect/notAttempted
+ *    }
+ * ]
+ */
 
-          
-        
+const TotalTime = 60;
+const DefaultState = {
+    selectedParagraph: "Hello World!",
+    testInfo: [],
+    timerStarted: false,
+    timeRemaining: TotalTime,
+    words: 0,
+    characters: 0,
+    wpm: 0,
+};
+
+class App extends React.Component {
+    // state = {
+    //     selectedParagraph: "Hello World!",
+    //     testInfo: [],
+    //     timerStarted: false,
+    //     timeRemaining: TotalTime,
+    //     words: 0,
+    //     characters: 0,
+    //     wpm: 0,
+    // };
+    state = DefaultState;
+
+    fetchNewParagraphFallback = () => {
+        const data =
+            SAMPLE_PARAGRAPHS[
+                Math.floor(Math.random() * SAMPLE_PARAGRAPHS.length)
+            ];
+
+        const selectedParagraphArray = data.split("");
+        const testInfo = selectedParagraphArray.map((selectedLetter) => {
+            return {
+                testLetter: selectedLetter,
+                status: "notAttempted",
+            };
+        });
+
+        // Update the testInfo in state
+        this.setState({
+            ...DefaultState,
+            selectedParagraph: data,
+            testInfo,
+        });
     };
 
-     
-componentDidMount() {
-    // fetch( apiURl)
-    // .then(response => response.text())
-    // .then((data) => {
-    //     // console.log(data)
-    //     this.setState({selectedParagraph : data})
-    // })
+    fetchNewParagraph = () => {
+        fetch("http://metaphorpsum.com/paragraphs/1/9")
+            .then((response) => response.text())
+            .then((data) => {
+                // Once the api results are here, break the selectedParagraph into test info
+                const selectedParagraphArray = data.split("");
+                const testInfo = selectedParagraphArray.map(
+                    (selectedLetter) => {
+                        return {
+                            testLetter: selectedLetter,
+                            status: "notAttempted",
+                        };
+                    }
+                );
 
-    const selectedParagraphArray = this.state.selectedParagraph.split("")
+                // Update the testInfo in state
+                this.setState({
+                    ...DefaultState,
+                    selectedParagraph: data,
+                    testInfo,
+                });
+            });
+    };
 
-    const testInfo = selectedParagraphArray.map(selectedLetter => {
-        return {
-            testLetter: selectedLetter,
-            status: "notAttempted"
+    componentDidMount() {
+        // As soon as the component mounts, load the selected paragraph from the API
+        this.fetchNewParagraphFallback();
+    }
+
+    startAgain = () => this.fetchNewParagraphFallback();
+
+    startTimer = () => {
+        this.setState({ timerStarted: true });
+        const timer = setInterval(() => {
+            if (this.state.timeRemaining > 0) {
+                // Change the WPM and Time Remaining
+                const timeSpent = TotalTime - this.state.timeRemaining;
+                const wpm =
+                    timeSpent > 0
+                        ? (this.state.words / timeSpent) * TotalTime
+                        : 0;
+                this.setState({
+                    timeRemaining: this.state.timeRemaining - 1,
+                    wpm: parseInt(wpm),
+                });
+            } else {
+                clearInterval(timer);
+            }
+        }, 1000);
+    };
+
+    handleUserInput = (inputValue) => {
+        if (!this.state.timerStarted) this.startTimer();
+
+        /**
+         * 1. Handle the underflow case - all characters should be shown as not-attempted
+         * 2. Handle the overflow case - early exit
+         * 3. Handle the backspace case
+         *      - Mark the [index+1] element as notAttempted
+         *        (irrespective of whether the index is less than zero)
+         *      - But, don't forget to check for the overflow here
+         *        (index + 1 -> out of bound, when index === length-1)
+         * 4. Update the status in test info
+         *      1. Find out the last character in the inputValue and it's index
+         *      2. Check if the character at same index in testInfo (state) matches
+         *      3. Yes -> Correct
+         *         No  -> Incorrect (Mistake++)
+         * 5. Irrespective of the case, characters, words and wpm can be updated
+         */
+
+        const characters = inputValue.length;
+        const words = inputValue.split(" ").length;
+        const index = characters - 1;
+
+        if (index < 0) {
+            this.setState({
+                testInfo: [
+                    {
+                        testLetter: this.state.testInfo[0].testLetter,
+                        status: "notAttempted",
+                    },
+                    ...this.state.testInfo.slice(1),
+                ],
+                characters,
+                words,
+            });
+
+            return;
         }
-    })
-    this.setState({testInfo})
-}
 
-handleUserInput = (inputValue) => {
+        if (index >= this.state.selectedParagraph.length) {
+            this.setState({
+                characters,
+                words,
+            });
+            return;
+        }
 
-}
+        // Make a copy
+        const testInfo = this.state.testInfo;
+        if (!(index === this.state.selectedParagraph.length - 1))
+            testInfo[index + 1].status = "notAttempted";
 
+        // Check for mistake
+        const isMistake = inputValue[index] === testInfo[index].testLetter;
 
-    render() { 
-        console.log(selectedParagraph)
+        // Update the testInfo
+        testInfo[index].status = isMistake ? "correct" : "incorrect";
 
-        console.log("test infor", this.state.testInfo)
-        
-        return (  
-            // jsx is nothing html inside the JS 
-            <div className="App" >
-                <Nav/>
-                <Landing/>
+        // Update the state
+        this.setState({
+            testInfo,
+            words,
+            characters,
+        });
+    };
+
+    render() {
+        return (
+            <div className="app">
+                <Nav />
+                <Landing />
                 <ChallengeSection
-                 selectedParagraph={this.state.selectedParagraph}
-                 timerStarted ={this.state.timerStarted}
-                 words= {this.state.words}
-                 characters= {this.state.characters}
-                 wpm ={this.state.wpm}
-                 timeRemaining={this.state.timeRemaining}
-                 testInfo={this.state.testInfo}
-
-                 onInputChange= {this.handleUserInput}
-                 
-                 
-                 />
-                <Footer/>
+                    selectedParagraph={this.state.selectedParagraph}
+                    testInfo={this.state.testInfo}
+                    onInputChange={this.handleUserInput}
+                    words={this.state.words}
+                    characters={this.state.characters}
+                    wpm={this.state.wpm}
+                    timeRemaining={this.state.timeRemaining}
+                    timerStarted={this.state.timerStarted}
+                    startAgain={this.startAgain}
+                />
+                <Footer />
             </div>
         );
     }
 }
- 
+
 export default App;
